14ef3a1a9740e2644e73dd799fe3a517a97b0168
Fabien Chouteau <chouteau@adacore.com>
2017-05-11 18:19:21 +0200

    Start low level steering control

---
+++ software/board_support/src/board-steering.adb
@@ -0,0 +1,89 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                                  O'PAVES                                 --
+--                                                                          --
+--                        Copyright (C) 2017, AdaCore                       --
+--                                                                          --
+-- This program is free software; you can redistribute it and/or modify it  --
+-- under terms of the GNU General Public License as published by the  Free  --
+-- Software  Foundation;  either version 3,  or (at your option) any later  --
+-- version. This software is distributed in the hope that it will be useful --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+with STM32.PWM;    use STM32.PWM;
+with HAL;          use HAL;
+
+package body Board.Steering is
+
+   Init_Done : Boolean := False;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize is
+   begin
+      Configure_PWM_Timer (PWM_Timer'Access, PWM_Frequency);
+
+      Modulator.Attach_PWM_Channel
+        (PWM_Timer'Access,
+         PWM_Channel,
+         PWM_Pin,
+         PWM_Pin_AF);
+
+      Init_Done := True;
+
+      --  Initial state
+      Disable;
+      Set_Steering (0.0);
+   end Initialize;
+
+   -----------------
+   -- Initialized --
+   -----------------
+
+   function Initialized return Boolean
+   is (Init_Done);
+
+   -------------
+   -- Enabled --
+   -------------
+
+   function Enabled return Boolean
+   is (Modulator.Output_Enabled);
+
+   ------------
+   -- Enable --
+   ------------
+
+   procedure Enable is
+   begin
+      Modulator.Enable_Output;
+   end Enable;
+
+   -------------
+   -- Disable --
+   -------------
+
+   procedure Disable is
+   begin
+      Modulator.Disable_Output;
+   end Disable;
+
+   ------------------
+   -- Set_Steering --
+   ------------------
+
+   procedure Set_Steering (Value : Steering_Value) is
+      Ms : constant Float := Center_Pulse_MS +
+        Float (Value) * Range_Pulse_MS / 2.0;
+   begin
+      Modulator.Set_Duty_Time (Microseconds (Ms * 1000.0));
+   end Set_Steering;
+end Board.Steering;
---
+++ software/board_support/src/board-steering.ads
@@ -0,0 +1,75 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                                  O'PAVES                                 --
+--                                                                          --
+--                        Copyright (C) 2017, AdaCore                       --
+--                                                                          --
+-- This program is free software; you can redistribute it and/or modify it  --
+-- under terms of the GNU General Public License as published by the  Free  --
+-- Software  Foundation;  either version 3,  or (at your option) any later  --
+-- version. This software is distributed in the hope that it will be useful --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+private with STM32.GPIO;
+private with STM32.Timers;
+private with STM32.PWM;
+private with STM32.Device;
+
+package Board.Steering is
+
+   subtype Steering_Value is Float range -1.0 .. 1.0;
+
+   procedure Initialize
+     with Post => Initialized and then not Enabled;
+   --  Initialize hardware for steering control
+
+   function Initialized return Boolean;
+   --  Return True if the hardware is initialized and steering ready to use.
+
+   function Enabled return Boolean
+     with Pre => Initialized;
+   --  Return True if steering is enabled
+
+   procedure Enable
+     with Pre => Initialized;
+
+   procedure Disable
+     with Pre => Initialized;
+
+   procedure Set_Steering (Value : Steering_Value);
+private
+
+   --  These are value for O'PAVES Mk-I rev-A which has a error in steering
+   --  servo connection. We temporarly use one pin of the Arduino header to
+   --  control the servo
+
+   PWM_Pin     : STM32.GPIO.GPIO_Point         renames STM32.Device.PD13;
+   PWM_Pin_AF  : STM32.GPIO_Alternate_Function renames STM32.Device.GPIO_AF_TIM4_2;
+
+   PWM_Timer   : STM32.Timers.Timer            renames STM32.Device.Timer_4;
+   PWM_Channel : STM32.Timers.Timer_Channel         := STM32.Timers.Channel_2;
+   PWM_Period  : constant                           := 1000;
+
+   PWM_Frequency : constant := 20; -- Hertz
+   Modulator     : STM32.PWM.PWM_Modulator;
+
+   Min_Pulse_MS : constant := 1.0;
+   --  Minimum uptime for the PWM pulse. Corresponds to servo's maximum
+   --  counter-clockwise movement.
+   Max_Pulse_Ms : constant := 2.0;
+   --  Maximum uptime for the PWM pulse. Corresponds to servo's maximum
+   --  clockwise movement.
+
+   Range_Pulse_MS : constant := Max_Pulse_Ms - Min_Pulse_MS;
+   --  Difference between the max and min PWM pulse
+
+   Center_Pulse_MS : constant := Min_Pulse_MS + Range_Pulse_MS / 2.0;
+   --  PWM uptime to set the servo in its center posistion
+
+end Board.Steering;
